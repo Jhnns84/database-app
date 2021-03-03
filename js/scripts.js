5a2ce779ee32abe141c79f9b74e2e5a8cfc5e1bb
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-03-03 19:12:23 +0100

    added... pokemon? ¯\_(ツ)_/¯

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert('Hello world');
-
-let favoriteFood = 'Cardboard';
-document.write(favoriteFood);
+let pokemonList = [{name: "houndour", height: 2, type: ["dark","fire"]},{name: "butterfree", height: 3, type: ["bug","flying"]},{name: "venusaur", height: 6, type: ["grass","poison"]}];
