8a6299ea027c19e3c1bb73ac611ef597d3ea498b
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-03-05 10:53:37 +0100

    added for-loop and height-condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-let pokemonList = [{name: "houndour", height: 2, type: ["dark","fire"]},{name: "butterfree", height: 3, type: ["bug","flying"]},{name: "venusaur", height: 6, type: ["grass","poison"]}];
+let pokemonList = [
+  {
+    name: "Houndour",
+    height: 2,
+    type: ["dark","fire"]
+  },
+  {
+    name: "Butterfree",
+    height: 3,
+    type: ["bug","flying"]
+  },
+  {
+    name: "Venusaur",
+    height: 6,
+    type: ["grass","poison"]
+  }
+];
+for (let i = 0; i < pokemonList.length; i++) {
+  // this code checks for height above 4 and adds a highlight (wow, that's big) to the pokemon
+  if (pokemonList[i].height > 4){
+  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big</p>');
+  }
+  // this code lists all pokemon with a height lower than 4
+  else if (pokemonList[i].height < 4){
+  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>');
+  }
+}
