b70761272b252209c4640f235d19995e8d495237
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-03-09 10:40:54 +0100

    Changed from string concatenations to template literals

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,10 @@ let pokemonList = [
 for (let i = 0; i < pokemonList.length; i++) {
   // this code checks for height above 4 and adds a highlight (wow, that's big) to the pokemon
   if (pokemonList[i].height > 4){
-  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' - Wow, that\'s big</p>');
+  document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big</p>`);
   }
   // this code lists all pokemon with a height lower than 4
   else if (pokemonList[i].height < 4){
-  document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>');
+  document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
   }
 }
