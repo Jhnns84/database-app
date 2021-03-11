d3aaf22305611d7eedac9d1a07f995b5a8c18820
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-03-11 11:29:02 +0100

    changed to forEach-function

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,14 @@ let pokemonList = [
     type: ["grass","poison"]
   }
 ];
-for (let i = 0; i < pokemonList.length; i++) {
+
+pokemonList.forEach(function(pokemon) {
   // this code checks for height above 4 and adds a highlight (wow, that's big) to the pokemon
-  if (pokemonList[i].height > 4){
-  document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big</p>`);
+  if (pokemon.height > 4){
+  document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big</p>`);
   }
   // this code lists all pokemon with a height lower than 4
-  else if (pokemonList[i].height < 4){
-  document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})</p>`);
+  else if (pokemon.height < 4){
+  document.write(`<p>${pokemon.name} (height: ${pokemon.height})</p>`);
   }
-}
+})
