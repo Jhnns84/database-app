ef17ac50c70df9a795fc470ae079fe93cabf3a64
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-03-11 15:34:34 +0100

    Added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
-let pokemonList = [
-  {
-    name: "Houndour",
-    height: 2,
-    type: ["dark","fire"]
-  },
-  {
-    name: "Butterfree",
-    height: 3,
-    type: ["bug","flying"]
-  },
-  {
-    name: "Venusaur",
-    height: 6,
-    type: ["grass","poison"]
-  }
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Houndour",
+      height: 2,
+      type: ["dark","fire"]
+    },
+    {
+      name: "Butterfree",
+      height: 3,
+      type: ["bug","flying"]
+    },
+    {
+      name: "Venusaur",
+      height: 6,
+      type: ["grass","poison"]
+    }
+  ];
 
-pokemonList.forEach(function(pokemon) {
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  }
+
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
   // this code checks for height above 4 and adds a highlight (wow, that's big) to the pokemon
   if (pokemon.height > 4){
   document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big</p>`);
