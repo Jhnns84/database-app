57da63344694b04d9bfb3f0d9aa10a4a3154f210
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-03-15 18:26:41 +0100

    created addListItem and used forEach to create a button for each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -25,20 +25,34 @@ let pokemonRepository = (function () {
     pokemonList.push(pokemon);
   }
 
+  function addListItem(pokemon) {
+    let pokeVariable = document.querySelector('ul');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText=pokemon.name;
+    button.classList.add('pokeButton');
+    listItem.appendChild(button);
+    pokeVariable.appendChild(listItem);
+  }
+
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem
   }
+
 
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon)
+
   // this code checks for height above 4 and adds a highlight (wow, that's big) to the pokemon
-  if (pokemon.height > 4){
-  document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big</p>`);
-  }
-  // this code lists all pokemon with a height lower than 4
-  else if (pokemon.height < 4){
-  document.write(`<p>${pokemon.name} (height: ${pokemon.height})</p>`);
-  }
+  // if (pokemon.height > 4){
+  // document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - Wow, that's big</p>`);
+  // }
+  // // this code lists all pokemon with a height lower than 4
+  // else if (pokemon.height < 4){
+  // document.write(`<p>${pokemon.name} (height: ${pokemon.height})</p>`);
+  // }
 })
