c1b3f04859c35b2958a3247384e0f29f9a837a58
Jhnns84 <77455115+Jhnns84@users.noreply.github.com>
2021-03-20 11:41:32 +0100

    added modal for pokemon-details

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+  let modalContainer = document.querySelector('#modal-container');
 
   function add(pokemon) {
     if (
@@ -61,11 +62,49 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(item) {
-    pokemonRepository.loadDetails(item).then(function () {
-      console.log(item);
-    });
+    modalContainer.innerHTML = '';
+
+    // this creates a new modal
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // this creates the content elements of the modal
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    // this event listener listens for clicks on the close button when the modal is visible
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = item.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = item.height;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
   }
 
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
   return {
     add: add,
